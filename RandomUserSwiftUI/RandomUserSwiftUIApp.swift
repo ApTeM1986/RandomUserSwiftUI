a2d1b3a7459c2820db5e4269e9b518bf37aab068
ApTeM1986 <saartem@gmail.com>
2022-11-28 13:13:41 +0100

    Initial Commit

---
+++ RandomUserSwiftUI/RandomUserSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserSwiftUIApp.swift
+//  RandomUserSwiftUI
+//
+//  Created by Artem Stetsenko on 28.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
